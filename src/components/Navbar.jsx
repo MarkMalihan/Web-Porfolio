9b42dd1006f4840c8beab1016aa4a197b218311e
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2024-01-28 23:52:36 +0800

    navbar active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useMatch } from "react-router-dom";
 
 function Navbar() {
+  const isHomeActive = useMatch("/");
+  const isProjectsActive = useMatch("/Projects");
+  const isContactActive = useMatch("/Contact");
   return (
     <>
       <nav className="sticky top-0 z-10 p-5 flex flex-col md:flex-row items-center justify-between bg-gray-800 text-white">
         <h1 className="text-xl font-bold tracking-[0.2em]">ARGEL MALIHAN</h1>
 
-        <ul className="flex gap-5 mt-5 md:mt-0">
-          <li className="hover:text-yellow-300">
+        <ul className="flex gap-6 mt-5 md:mt-0">
+          <li
+            className={`px-4 py-1 rounded-full hover:bg-blue-200 hover:text-black/80 ${
+              isHomeActive
+                ? "bg-blue-200 px-4 py-1 rounded-full text-black/80"
+                : ""
+            }`}
+          >
             <NavLink to="/">Home</NavLink>
           </li>
 
-          <li className="hover:text-blue-300">
+          <li
+            className={`px-4 py-1 rounded-full hover:bg-blue-200 hover:text-black/80 ${
+              isProjectsActive
+                ? "bg-blue-200 px-4 py-1 rounded-full text-black/80"
+                : ""
+            }`}
+          >
             <NavLink to="/Projects">Project</NavLink>
           </li>
 
-          <li className="hover:text-green-300">
+          <li
+            className={`px-4 py-1 rounded-full hover:bg-blue-200 hover:text-black/80 ${
+              isContactActive
+                ? "bg-blue-200 px-4 py-1 rounded-full text-black/80"
+                : ""
+            }`}
+          >
             <NavLink to="/Contact">Contact</NavLink>
           </li>
         </ul>
