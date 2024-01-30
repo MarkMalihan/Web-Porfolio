7b50c38110ef4ee3a7178df1aee09a2037ad061c
Mark Angelo Malihan <malihanmarkangelolizano@gmail.com>
2024-01-30 18:44:24 +0800

    timeline effects

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,7 +4,7 @@ function Timeline() {
   return (
     <>
       <section className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
-        <div className="p-3 border rounded-md space-y-5">
+        <div className="p-3 border rounded-md space-y-5 hover:scale-105 duration-300">
           <h1 className="text-2xl text-white font-bold">April 2023</h1>
           <hr />
           <p className="text-gray-300">
@@ -13,7 +13,7 @@ function Timeline() {
           </p>
         </div>
 
-        <div className="p-3 border rounded-md space-y-5">
+        <div className="p-3 border rounded-md space-y-5 hover:scale-105 duration-300">
           <h1 className="text-2xl text-white font-bold">October 2023</h1>
           <hr />
           <p className="text-gray-300">
@@ -22,7 +22,7 @@ function Timeline() {
           </p>
         </div>
 
-        <div className="p-3 border rounded-md space-y-5">
+        <div className="p-3 border rounded-md space-y-5 hover:scale-105 duration-300">
           <h1 className="text-2xl text-white font-bold">October 2023</h1>
           <hr />
           <p className="text-gray-300">
